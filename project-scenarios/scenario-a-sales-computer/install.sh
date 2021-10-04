4f7ba1281f06a2c3f52f084c42144ca2130e8d30
Pedro <pedro.correia@academiadecodigo.com>
2021-10-04 19:19:34 +0100

    add scenario a script

---
+++ project-scenarios/scenario-a-sales-computer/install.sh
@@ -0,0 +1,10 @@
+#!bin/bash
+
+#Install VSCode
+wget -q https://packages.microsoft.com/keys/microsoft.asc -O- | sudo apt-key add -
+sudo add-apt-repository "deb [arch=amd64] https://packages.microsoft.com/repos/vscode stable main"
+sudo apt update
+sudo apt install code
+
+#Install Thunderbird
+sudo apt install thunderbird
