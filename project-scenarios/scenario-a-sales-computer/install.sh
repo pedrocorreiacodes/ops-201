4f2f480f9c3e009ad3597c42beb561312af39236
Pedro <pedro.correia@academiadecodigo.com>
2022-01-18 14:38:47 +0000

    update scripts

--- project-scenarios/scenario-a-sales-computer/install.sh
+++ project-scenarios/scenario-a-sales-computer/install.sh
@@ -1,10 +1,22 @@
 #!bin/bash
+sudo apt update
 
 #Install VSCode
 wget -q https://packages.microsoft.com/keys/microsoft.asc -O- | sudo apt-key add -
 sudo add-apt-repository "deb [arch=amd64] https://packages.microsoft.com/repos/vscode stable main"
-sudo apt update
 sudo apt install code
 
 #Install Thunderbird
-sudo apt install thunderbird
+sudo apt-get install thunderbird
+
+#Set Desktop Wallpaper
+SRC_URI="https://taipricebook.es/wp-content/uploads/2020/09/arrow-qumulo-logo-og-web.jpg"
+FNAME="/home/$USER/Pictures/desktop.png"
+wget "$SRC_URI" -O "$FNAME"
+gsettings set org.gnome.desktop.background picture-uri "$FNAME"
+
+#launch Thunderbird
+thunderbird
+
+#launch vscode
+code
